638d20a03cc0a543d276fc27bb80b4dec4653ec2
Tung Yu Chen <james19900311@gmail.com>
2022-03-29 15:03:24 -0700

    Add entities for movie

---
+++ FinaiProejct_200OK/Entities/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinaiProejct_200OK.Entities
+{
+    class Genre
+    {
+    }
+}
---
+++ FinaiProejct_200OK/Entities/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinaiProejct_200OK.Entities
+{
+    class Movie
+    {
+        public int MovieId { get; set; }
+        public string MovieTitle { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+        public virtual List<Genre> Genres { get; set; }
+        public virtual Detail MovieDetail { get; set; }
+        public virtual Director MovieDirector { get; set; }
+    }
+}
