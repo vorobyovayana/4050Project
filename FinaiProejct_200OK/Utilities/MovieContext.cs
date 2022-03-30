9ace9f02a976a67e1febb6dc5abf46cbab4ce6f3
Tung Yu Chen <james19900311@gmail.com>
2022-03-29 17:20:46 -0700

    create Dbcontext and install relative package

---
+++ FinaiProejct_200OK/Utilities/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using FinaiProejct_200OK.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinaiProejct_200OK.Utilities
+{
+    class MovieContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
