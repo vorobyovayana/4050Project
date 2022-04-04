f4e58382159f285ea7e48e386a63c2e29a062c3f
Tung Yu Chen <james19900311@gmail.com>
2022-04-03 20:08:10 -0700

    create login function

--- FinaiProejct_200OK/Utilities/LoginMethod.cs
+++ FinaiProejct_200OK/Utilities/LoginMethod.cs
@@ -8,5 +8,22 @@ namespace FinaiProejct_200OK.Utilities
 {
     class LoginMethod
     {
+        public static int Login(string userName, string passwrod)
+        {
+            // Search userName in table
+
+
+            string userPassword = ""; // From database
+            string key = ""; // From database
+            int userId = 0; // From database
+            // Compare the password
+            if (passwrod == Encrypt.DecryptPassword(key, userPassword))
+            {
+                return userId;
+            } else
+            {
+                return -1;
+            }
+        }
     }
 }
